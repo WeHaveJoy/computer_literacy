74f1b03bb4a1851743472941524cfa4bb9fce593
Sinovuyo Dyantyi <cnovuyo10@gmail.com>
2022-07-11 12:27:10 +0200

    added the tables file and the link on deploy.sh

--- client/deploy.sh
+++ client/deploy.sh
@@ -19,7 +19,7 @@ git add -A
 git commit -m 'deploy'
 
 # Add your GitHub repo details here
-git push -f  main:gh-pages
+git push -f https://github.com/WeHaveJoy/computer_literacy.git main:gh-pages
 
 # if you are deploying to https://WeHaveJoy.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
