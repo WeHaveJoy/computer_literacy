62920349a75389ea6687993d62324b06868ca98b
Sinovuyo Dyantyi <cnovuyo10@gmail.com>
2022-07-14 23:13:36 +0200

    added descriptions into the database

--- server/sql/data.sql
+++ server/sql/data.sql
@@ -1,12 +1,25 @@
-insert into courses_beginners(description, img, level) values ('', 'mouse.jpeg', '1');
-insert into courses_beginners(description, img, level) values ('', 'computer box.jpeg', '1');
-insert into courses_beginners(description, img, level) values ('', 'monitor.jpeg', '1');
+insert into courses_beginners(description, img, level) values ('A mouse is an input device that allows the user to move a pointer displayed on the monitor and experience a more intuitive interaction with computer systems.', 'mouse.jpeg', '1');
+
+insert into courses_beginners(description, img, level) values ('This is the part that holds all of the parts of a computer to make up the computer system.
+It is usually designed in such a manner to make fitting a motherboard, wiring, and drives as easy as possible. Some are designed so well that it is easy to make everything look tidy and presentable too.
+Cases come in all different sizes and shapes to accommodate various types of computer components and satisfy the consumer’s needs.', 'computer box.jpeg', '1');
+
+insert into courses_beginners(description, img, level) values ('A monitor is an output device used to visualize the graphics data sent from the computer’s GPU.
+There are various types of monitors on the market. A LED (Light Emitting Diode) backlit LCD (Liquid Crystal Display) monitor is the most commonly used with modern computers.', 'monitor.jpeg', '1');
+
 insert into courses_beginners(description, img, level) values ('', 'USB cable.jpeg', '1');
+
 insert into courses_beginners(description, img, level) values ('', 'USB port.jpeg', '1');
+
 insert into courses_beginners(description, img, level) values ('', 'HDMI-cable.jpeg', '1');
+
 insert into courses_beginners(description, img, level) values ('', 'HDMI to VGA adapter.jpeg', '1');
 insert into courses_beginners(description, img, level) values ('', 'keyboard.jpeg', '1');
-insert into courses_beginners(description, img, level) values ('', 'computer-motherboard.jpeg', '1');
+
+insert into courses_beginners(description, img, level) values ('The motherboard is the main board that is screwed directly inside the computer case. All other cards and everything else plugs directly into the motherboard, hence its name.
+The CPU, RAM, drives, power supply, and more are connected to it.
+Its function involves integrating all the physical components to communicate and operate together.', 'computer-motherboard.jpeg', '1');
+
 insert into courses_beginners(description, img, level) values ('', 'hdmi port.jpeg', '1');
 
 
